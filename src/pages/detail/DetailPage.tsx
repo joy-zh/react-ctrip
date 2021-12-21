254ac1ce880547033cb7a99b2ff82d0162e0d918
zhangyi1214 <zhangyi1214@bailongma-inc.com>
2021-12-21 10:45:32 +0800

    detail page

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styles from './DetailPage.module.css'
-import { RouteComponentProps } from 'react-router-dom'
+import { RouteComponentProps, useParams } from 'react-router-dom'
+import axios from 'axios'
 
 interface MatchParams {
   touristRouteId: string;
 }
 
 export const DetailPage: React.FC<RouteComponentProps<MatchParams>> = (props) => {
+  const {touristRouteId} = useParams<MatchParams>()
+  const [loading, setLoading] = useState(true)
+  const [product, setProduct] = useState(null)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true)
+      await axios.get(``)
+    }
+    fetchData()
+  }, [])
+
   return <h1>路线详情页面，路线ID：{props.match.params.touristRouteId}</h1>
 }
